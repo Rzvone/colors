c2c5691d25e26428f8bc152bbd62b1f47f02222a
Rzvone <bobonea.razvan.ctin@gmail.com>
2023-12-05 17:58:25 +0200

    added promos

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -23,6 +23,7 @@ export const dynamic = 'force-dynamic'
 
 import classes from './index.module.scss'
 import Categories from '../../_components/Categories'
+import Promotion from '../../_components/Promotion'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -66,6 +67,7 @@ export default async function Page({ params: { slug = 'home' } }) {
 
           <Gutter className={classes.home}>
             <Categories categories={categories}/>
+            <Promotion />
           </Gutter>
         </section>
       ): (
