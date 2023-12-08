9642e7169d77904e461dc16b8e839a9b9ad171f4
Rzvone <bobonea.razvan.ctin@gmail.com>
2023-12-08 21:13:03 +0200

    made paymeng do and add order to database

--- src/app/(pages)/checkout/CheckoutForm/index.tsx
+++ src/app/(pages)/checkout/CheckoutForm/index.tsx
@@ -26,79 +26,57 @@ export const CheckoutForm: React.FC<{}> = () => {
       setIsLoading(true)
 
       try {
-        const { error: stripeError, paymentIntent } = await stripe?.confirmPayment({
-          elements: elements!,
-          redirect: 'if_required',
-          confirmParams: {
-            return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/order-confirmation`,
+        // Create the order in the database
+        const orderReq = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/orders`, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
           },
+          body: JSON.stringify({
+            total: cartTotal.raw,
+            items: (cart?.items || [])?.map(({ product, quantity }) => ({
+              product: typeof product === 'string' ? product : product.id,
+              quantity,
+              price:
+                typeof product === 'object' ? priceFromJSON(product.priceJSON, 1, true) : undefined,
+            })),
+          }),
         })
 
-        if (stripeError) {
-          setError(stripeError.message)
-          setIsLoading(false)
-        }
+        if (!orderReq.ok) throw new Error(orderReq.statusText || 'Something went wrong.')
 
-        if (paymentIntent) {
-          // Before redirecting to the order confirmation page, we need to create the order in Payload
-          // Cannot clear the cart yet because if you clear the cart while in the checkout
-          // you will be redirected to the `/cart` page before this redirect happens
-          // Instead, we clear the cart in an `afterChange` hook on the `orders` collection in Payload
-          try {
-            const orderReq = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/orders`, {
-              method: 'POST',
-              credentials: 'include',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                total: cartTotal.raw,
-                stripePaymentIntentID: paymentIntent.id,
-                items: (cart?.items || [])?.map(({ product, quantity }) => ({
-                  product: typeof product === 'string' ? product : product.id,
-                  quantity,
-                  price:
-                    typeof product === 'object'
-                      ? priceFromJSON(product.priceJSON, 1, true)
-                      : undefined,
-                })),
-              }),
-            })
+        const {
+          error: errorFromRes,
+          doc,
+        }: {
+          message?: string
+          error?: string
+          doc: Order
+        } = await orderReq.json()
 
-            if (!orderReq.ok) throw new Error(orderReq.statusText || 'Something went wrong.')
+        if (errorFromRes) throw new Error(errorFromRes)
 
-            const {
-              error: errorFromRes,
-              doc,
-            }: {
-              message?: string
-              error?: string
-              doc: Order
-            } = await orderReq.json()
+        // Open WhatsApp with a pre-filled message
+        const phoneNumber = '+40729590667' // replace with the phone number you want to send the message to
+        const message = `Order #${doc.id} has been placed.`
+        const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
+        window.open(whatsappUrl)
 
-            if (errorFromRes) throw new Error(errorFromRes)
-
-            router.push(`/order-confirmation?order_id=${doc.id}`)
-          } catch (err) {
-            // don't throw an error if the order was not created successfully
-            // this is because payment _did_ in fact go through, and we don't want the user to pay twice
-            console.error(err.message) // eslint-disable-line no-console
-            router.push(`/order-confirmation?error=${encodeURIComponent(err.message)}`)
-          }
-        }
+        router.push(`/order-confirmation?order_id=${doc.id}`)
       } catch (err) {
         const msg = err instanceof Error ? err.message : 'Something went wrong.'
-        setError(`Error while submitting payment: ${msg}`)
+        setError(`Error while submitting order: ${msg}`)
         setIsLoading(false)
       }
     },
-    [stripe, elements, router, cart, cartTotal],
+    [router, cart, cartTotal],
   )
 
   return (
     <form onSubmit={handleSubmit} className={classes.form}>
       {error && <Message error={error} />}
-      <PaymentElement />
+      {/* <PaymentElement /> */}
       <div className={classes.actions}>
         <Button label="Back to cart" href="/cart" appearance="secondary" />
         <Button
